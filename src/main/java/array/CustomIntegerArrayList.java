c3595bb59ff8cddf5ce0e73e4f1efade341a9ce8
Hero <81640511+icraft2170@users.noreply.github.com>
2022-06-04 04:02:28 +0900

    [ Custom Integer ArrayList ] - 가변 배열 - 순서 존재
    ## 기능
    - add(int data)
    - Integer get(int index)
    - remove(int idex)
    - int size()
    - clear()

--- src/main/java/array/CustomIntegerArrayList.java
+++ src/main/java/array/CustomIntegerArrayList.java
@@ -1,23 +1,28 @@
 package array;
 
 public class CustomIntegerArrayList {
-    private int[] list;
+    private Integer[] list;
     private int currentIndex;
 
     public CustomIntegerArrayList() {
-        this.list = new int[10];
+        this.list = new Integer[10];
         this.currentIndex = 0;
     }
 
     public void add(int data) {
-        if (this.currentIndex == list.length) {
-            int[] tmpList = list;
-            this.list = new int[list.length + 10];
+        if (this.currentIndex == (list.length - 1)) {
+            Integer[] tmpList = list;
+            this.list = new Integer[list.length + 10];
             for (int i = 0; i < tmpList.length; i++) {
                 list[i] = tmpList[i];
             }
         }
-        this.list[++currentIndex] = data;
+        this.list[currentIndex++] = data;
+    }
+
+    public Integer get(int index) {
+        if (index < 0) throw new IllegalArgumentException("Index cannot be less than zero.");
+        return list[index];
     }
 
     public void remove(int index) {
@@ -26,12 +31,16 @@ public class CustomIntegerArrayList {
         }
     }
 
-    public boolean isEmpty() {
-        return false;
+    public int size() {
+        int count = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) count++;
+        }
+        return count;
     }
 
     public void clear() {
-        this.list = new int[10];
+        this.list = new Integer[10];
     }
 
 
--- src/main/java/array/Main.java
+++ src/main/java/array/Main.java
@@ -1,12 +1,42 @@
 package array;
 
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> integers = new ArrayList<>();
-        integers.add(2);
-//        integers.
+        CustomIntegerArrayList customIntegerArrayList = new CustomIntegerArrayList();
+        customIntegerArrayList.add(1);
+        customIntegerArrayList.add(2);
+        customIntegerArrayList.add(3);
+        customIntegerArrayList.add(4);
+        customIntegerArrayList.add(5);
+        customIntegerArrayList.add(6);
+        customIntegerArrayList.add(7);
+        customIntegerArrayList.add(8);
+        customIntegerArrayList.add(9);
+        customIntegerArrayList.add(10);
+        customIntegerArrayList.add(11);
+
+
+
+        int size = customIntegerArrayList.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(customIntegerArrayList.get(i));
+        }
+
+        customIntegerArrayList.remove(10);
+
+        size = customIntegerArrayList.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(customIntegerArrayList.get(i));
+        }
+
+        customIntegerArrayList.clear();
+
+
+        size = customIntegerArrayList.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(customIntegerArrayList.get(i));
+        }
     }
 }
